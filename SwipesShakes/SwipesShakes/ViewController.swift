56065451d3128d1ae024ef2f64cca1f5c21d3289
Romildo Jozue Paiter <romildopaiter@gmail.com>
2016-04-16 14:36:03 -0400

    Aula 51: App que reconhece movimentos do usuário Cima, Baixo, Direita, Esquerda e Handing Shake

--- SwipesShakes/SwipesShakes/ViewController.swift
+++ SwipesShakes/SwipesShakes/ViewController.swift
@@ -10,15 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var actionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Swipe - Deslizamento de Dedo
+        // Direita
+        let swipeRigth = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swipe))
+        swipeRigth.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRigth)
+        
+        let swipeUP = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swipe))
+        swipeUP.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUP)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swipe))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swipe))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
+
+        
+    }
+    
+    func swipe(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                actionLabel.text = "Rigth"
+                print("Direita")
+            case UISwipeGestureRecognizerDirection.Up:
+                actionLabel.text = "Up"
+                print("Cima")
+            case UISwipeGestureRecognizerDirection.Down:
+                actionLabel.text = "Down"
+                print("Baixo")
+            case UISwipeGestureRecognizerDirection.Left:
+                actionLabel.text = "Left"
+                print("Esquerda")
+            default:
+                break
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+            actionLabel.text = "Device was shaking"
+            print("Device was shaking")
+        }
+    }
+    
+    
 
 
 }
