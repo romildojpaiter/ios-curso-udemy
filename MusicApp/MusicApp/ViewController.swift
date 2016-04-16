e84b7b6bc502aa9b8c4a01d0d9495c10a89e281b
Romildo Jozue Paiter <romildopaiter@gmail.com>
2016-04-16 01:32:23 -0400

    Projeto MusicApp, onde utilizamos uma interface mais elaborada e alguns recursos interessantes para o controle de audio

--- MusicApp/MusicApp/ViewController.swift
+++ MusicApp/MusicApp/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var scrubSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        executeMusic()
+        
+        // set o tempo do slider
+        scrubSlider.maximumValue = Float(player.duration)
+        
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
+    }
+    
+    func updateTime(){
+        scrubSlider.value = Float(player.currentTime)
+    }
+    
+    func executeMusic() {
+        do {
+            try
+                player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("david", ofType: "mp3")!))
+        } catch {
+            // work if erroe
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    /**
+     Metodos do aplicativo
+    */
+    @IBAction func playButton(sender: AnyObject) {
+        player.play()
+    }
+    
+    @IBAction func volumeAjust(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    
+    @IBAction func scrubAjust(sender: AnyObject) {
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+    }
 
+    @IBAction func pauseButton(sender: AnyObject) {
+        player.pause()
+    }
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        player.pause()
+        executeMusic()
+    }
 }
 
