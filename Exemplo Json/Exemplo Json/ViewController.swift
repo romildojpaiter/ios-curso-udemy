beac891ab19f80d46037f586490598844c47643a
Romildo Jozue Paiter <romildopaiter@gmail.com>
2016-06-17 00:06:09 -0400

    Exemplo JSON

--- Exemplo Json/Exemplo Json/ViewController.swift
+++ Exemplo Json/Exemplo Json/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblJsonResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let geoIp = NSURL(string: "http://geoip.nekudo.com/api/189.87.159.130")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(geoIp) { (data, response, error) in
+            
+            if let urlContent = data {
+                
+                do {
+                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
+                    
+                    print(jsonResult)
+                    print(jsonResult["country"]!)
+                    
+                    self.lblJsonResult.text = jsonResult["country"]! as? String
+                    
+                } catch {
+                    print("Json Serialization fail")
+                }
+                
+            } else {
+                print("error")
+            }
+        }
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
