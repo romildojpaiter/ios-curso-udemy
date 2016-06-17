e22973c06af8035c57ca25750d365f0e68b61289
Romildo Jozue Paiter <romildopaiter@gmail.com>
2016-06-17 00:06:37 -0400

    Web-View-Exeplo

--- Web-View-Exemplo/Web-View-Exemplo/ViewController.swift
+++ Web-View-Exemplo/Web-View-Exemplo/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "https://www.mirago.com.br/")!
+        
+        let request = NSURLRequest(URL: url)
+        
+        webView.loadRequest(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
