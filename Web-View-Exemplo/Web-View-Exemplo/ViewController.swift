90cd79587e09ae3db1e54ffd8bad105350fe6a26
Romildo Jozue Paiter <romildopaiter@gmail.com>
2016-06-17 00:03:14 -0400

    Baixando Imagens da Web

---
+++ Web-View-Exemplo/Web-View-Exemplo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web-View-Exemplo
+//
+//  Created by Romildo Jozue Paiter on 5/12/16.
+//  Copyright © 2016 Paiter App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
